1c8e232d063b36eacb0ff602f98d1a9cad6565fb
Shubham <shubhamsaini8965@gmail.com>
2024-09-07 21:35:42 +0530

    error fixed

--- src/app/courses/page.js
+++ src/app/courses/page.js
@@ -53,7 +53,7 @@ const page = () => {
                                     as="button"
                                     className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                                 >
-                                   Enroll Now
+                                   Enroll
                                 </CardItem>
                             </div>
                         </CardBody>
